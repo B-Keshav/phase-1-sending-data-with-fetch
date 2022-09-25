a5515f020b3a8fc79e2cc7cbe13a522769955d63
Keshav Bhati <keshavbhati2000@gmail.com>
2022-09-25 15:40:46 -0400

    commit tree 4fe7f44be66b05f6ea010c32f28d287401dafb90

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+function submitData(userName, userEmail){
+    
+    const configurationObject = {
+        method : "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name : userName,
+            email : userEmail
+        }),
+    };
+
+    let body = document.querySelector('body');
+
+    return fetch ("http://localhost:3000/users", configurationObject)
+    .then(function (res){
+        return res.json()
+    })
+    .then(function (data){
+        let enterName = document.createElement('h3');
+        let enterMail = document.createElement('h3');
+        let newID = document.createElement('p');
+        enterName.textContent = `Name : ${data.name}`;
+        enterMail.textContent = `EMail : ${data.email}`;
+        newID.textContent = `ID : ${data.id}`;
+        body.append(enterName, enterMail, newID);
+    })
+    .catch(function (error) {
+        let errr = document.createElement('h3');
+        errr.textContent = `Hey something went wrong, ${error}`;
+        body.append(errr);
+    });
+    
+
+
+}
